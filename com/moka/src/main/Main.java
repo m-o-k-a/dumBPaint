b30c4c0fb9acd50e06841e024300c13b94fdd6a5
Eddy Ikhlef <eddy.galdon@gmail.com>
2019-12-23 14:29:56 +0100

    lock resolution

--- com/moka/src/main/Main.java
+++ com/moka/src/main/Main.java
@@ -13,6 +13,7 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("../fxml/scene.fxml"));
         primaryStage.setTitle("dumBPaint - stupid painting software");
         primaryStage.setScene(new Scene(root, 1280, 720));
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
