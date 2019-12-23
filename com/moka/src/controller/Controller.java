664ee293bc3922dfa4241d240bae6545915b3111
Eddy Ikhlef <eddy.galdon@gmail.com>
2019-12-23 14:30:44 +0100

    Added menu bar

--- com/moka/src/controller/Controller.java
+++ com/moka/src/controller/Controller.java
@@ -1,4 +1,14 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuBar;
+import javafx.scene.input.MouseEvent;
+
 public class Controller {
+    @FXML
+    private MenuBar menuBar;
+
+    public void menuOpacityFalse(MouseEvent mouseEvent) { menuBar.setStyle("-fx-opacity: 0.8; -fx-border-color: transparent;"); }
+
+    public void menuOpacitytrue(MouseEvent mouseEvent) { menuBar.setStyle("-fx-opacity: 0.4; -fx-border-color: transparent;"); }
 }
