ca05c178697f4ee09378ddcc8bb64d24964fedf2
Eddy Ikhlef <eddy.galdon@gmail.com>
2019-12-23 13:44:53 +0100

    initialize scene w/ a resolution of 1280*720

--- com/moka/src/main/Main.java
+++ com/moka/src/main/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("../fxml/scene.fxml"));
         primaryStage.setTitle("dumBPaint - stupid painting software");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 1280, 720));
         primaryStage.show();
     }
 
