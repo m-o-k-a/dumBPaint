af2b8e582ec441f0f8cc221910016365d1241be0
Eddy Ikhlef <eddy.galdon@gmail.com>
2019-12-23 16:26:09 +0100

    Add more size options, Add shapes

--- com/moka/src/controller/Controller.java
+++ com/moka/src/controller/Controller.java
@@ -8,6 +8,7 @@ import javafx.scene.control.MenuBar;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
+import javafx.scene.shape.Polygon;
 
 public class Controller {
     @FXML
@@ -26,8 +27,32 @@ public class Controller {
 
     public void draw(MouseEvent mouseEvent) {
         gc = canvas.getGraphicsContext2D();
-        gc.setFill(color);
-        gc.fillRect(mouseEvent.getX(), mouseEvent.getY(),size, size);
+        if (brush == BrushEnum.ERASE) { gc.setFill(Color.WHITE); }
+        else { gc.setFill(color); }
+        drawShape(mouseEvent);
+    }
+
+    private void drawShape(MouseEvent mouseEvent) {
+        switch (brush) {
+            case PENCIL:
+                gc.fillRect(mouseEvent.getX(), mouseEvent.getY(),size, size); break;
+            case BUCKET:
+                gc.fillRect(canvas.getLayoutX(), canvas.getLayoutY(), canvas.getWidth(), canvas.getHeight()); break;
+            case ERASE:
+                gc.fillOval(mouseEvent.getX(), mouseEvent.getY(),size, size); break;
+            case SQUARE:
+                gc.fillRect(mouseEvent.getX(), mouseEvent.getY(),size, size); break;
+            case CIRCLE:
+                gc.fillOval(mouseEvent.getX(), mouseEvent.getY(),size, size); break;
+            case TRIANGLE:
+                double[] dotX = new double[4];
+                dotX[0] = mouseEvent.getX(); dotX[1] = mouseEvent.getX()+size; dotX[2] = mouseEvent.getX()-size; dotX[3] = mouseEvent.getX();
+                double[] dotY = new double[4];
+                dotY[0] = mouseEvent.getY(); dotY[1] = mouseEvent.getY()+size; dotY[2] = mouseEvent.getY()+size; dotY[3] = mouseEvent.getY();
+                gc.fillPolygon(dotX, dotY, 4);
+                break;
+        }
+
     }
 
     public void clear(ActionEvent actionEvent) {
@@ -39,6 +64,16 @@ public class Controller {
     public void setSmall(ActionEvent actionEvent) { size = 1; }
     public void setMedium(ActionEvent actionEvent) { size = 5; }
     public void setLarge(ActionEvent actionEvent) { size = 10; }
+    public void setXL(ActionEvent actionEvent) { size = 20; }
+    public void setXXL(ActionEvent actionEvent) { size = 40; }
+    public void setXXXL(ActionEvent actionEvent) { size = 60; }
+
+    public void setPencil(ActionEvent actionEvent) { brush = BrushEnum.PENCIL; }
+    public void setBucket(ActionEvent actionEvent) { brush = BrushEnum.BUCKET; }
+    public void setErase(ActionEvent actionEvent) { brush = BrushEnum.ERASE; }
+    public void setSquare(ActionEvent actionEvent) { brush = BrushEnum.SQUARE;}
+    public void setCircle(ActionEvent actionEvent) { brush = BrushEnum.CIRCLE; }
+    public void setTriangle(ActionEvent actionEvent) { brush = BrushEnum.TRIANGLE;}
 
     private enum BrushEnum {
         PENCIL,
